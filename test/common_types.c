0ef108d57de286a3ffe959f2e40b4165740fbec5
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2016-01-22 14:11:21 +0100

    more unit tests for common_types

--- test/common_types.c
+++ test/common_types.c
@@ -6,6 +6,19 @@
 void main()
 {
     assert(strcmp(pharmml_columnType_to_string(0), "undefined") == 0);
+    assert(strcmp(pharmml_columnType_to_string(PHARMML_COLTYPE_STRUCT_PARAMETER), "structParameter") == 0);
+
+    assert(pharmml_string_to_columnType("undefined") == 0);
+    assert(pharmml_string_to_columnType("varParameter_var") == PHARMML_COLTYPE_VAR_PARAMETER_VAR);
+
+    assert(strcmp(pharmml_valueType_to_string(PHARMML_VALUETYPE_STRING), "string") == 0);
+    assert(strcmp(pharmml_valueType_to_string(PHARMML_VALUETYPE_REAL), "real") == 0);
+
+    assert(strcmp(pharmml_valueType_to_element(PHARMML_VALUETYPE_STRING), "ct:String") == 0);
+    assert(strcmp(pharmml_valueType_to_element(PHARMML_VALUETYPE_INT), "ct:Int") == 0);
+
+    assert(pharmml_string_to_valueType("id") == PHARMML_VALUETYPE_ID);
+    assert(pharmml_string_to_valueType("int") == PHARMML_VALUETYPE_INT);
 
     printf("common_types PASS\n");
 }
