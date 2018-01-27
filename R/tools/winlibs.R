171cce9f896938c44f2841db6d4d4b4208879b4e
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2018-01-27 21:40:19 +0100

    R: Use win build server libxml2 if available

--- R/tools/winlibs.R
+++ R/tools/winlibs.R
@@ -1,5 +1,5 @@
-# Build against libxml2 from Rtools
-if (!file.exists("../windows/libxml2-2.9.4/include/libxml2/libxml/parser.h")) {
+# Build against libxml2 from Rtools. This is not be used on CRAN (see Makevars.win)
+if (!file.exists("../windows/libxml2-2.9.4/include/libxml2/libxml/parser.h") && Sys.getenv("LIB_XML") == "") {
   if(getRversion() < "3.3.0") setInternet2()
   download.file("https://github.com/rwinlib/libxml2/archive/v2.9.4.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
